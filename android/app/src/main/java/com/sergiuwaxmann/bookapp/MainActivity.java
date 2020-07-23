010dda059de6099fff0d02be96628c16d225448f
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-07-23 16:48:23 +0300

    Initial commit

---
+++ android/app/src/main/java/com/sergiuwaxmann/bookapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sergiuwaxmann.bookapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BookApp());
+
+class BookApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
